74c3afb66d5e186569d3fcdcab37e75e2d072330
Yousef eslam <yousefeslam214@gmail.com>
2023-11-29 00:59:07 +0200

    done

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
+# Author -Godswill Kalu
+
 def pow(a, b):
-    flag = 0
-    if b == 0:
-        return 1
-    if a < 0:
-        flag = 1
-    if b < 0:
-        for i in range(1, -b):
-            a *= a
-        return (1 / a)
-    for i in range(1, b):
-        a *= a
-    if flag == 1 and b % 2 != 0:
-        return -a
-    return a
+    return (a ** b)
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# Author - Godswill Kalu
+
 def islower(c):
-    if c >= 'a' and c <= 'z':
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
