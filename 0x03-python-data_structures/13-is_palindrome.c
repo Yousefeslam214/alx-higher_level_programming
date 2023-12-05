e65c4bdb6f619929a4880153adbdb60c50d6422b
Yousef eslam <yousefeslam214@gmail.com>
2023-12-05 10:36:55 +0200

    done

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,31 +2,30 @@
 #include <stdio.h>
 int is_palindrome(listint_t **head)
 {
-    int *arr;
-    int len= 0, i = 0, j;
-    listint_t *current;
-    current = *head;
-    while (current)
-    {
-        current = current->next;
-        len++;
-    }
-    arr = malloc(sizeof(int) * len);
-    if (!arr)
-        return (0);
-    for (i = 0; current; i++)
-    {
-        arr[i] = current->n;
-        current = current->next;
-    }
-    for (j = 0; j < ((len /2)); j++)
-    {
-        if (arr[j] != arr[len - j - 1])
-        {
-            free(arr);
-            return (0);
-            }
-    }
-    free(arr);
-    return (1);
+	int *arr, len= 0, i = 0, j;
+	listint_t *current;
+	current = *head;
+	while (current)
+	{
+		current = current->next;
+		len++;
+	}
+	arr = malloc(sizeof(int) * len);
+	if (!arr)
+		return (0);
+	for (i = 0; current; i++)
+	{
+		arr[i] = current->n;
+		current = current->next;
+	}
+	for (j = 0; j < ((len /2)); j++)
+	{
+		if (arr[j] != arr[len - j - 1])
+		{
+			free(arr);
+			return (0);
+			}
+	}
+	free(arr);
+	return (1);
 }
