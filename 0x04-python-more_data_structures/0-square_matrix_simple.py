8cf5c6d8b1c692a23b176908915f71198f6ddff9
Yousef eslam <yousefeslam214@gmail.com>
2023-12-20 02:40:14 +0200

    done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_mat = []
-    for i in matrix:
-        new_mat.append(list(map(lambda n: n * n, i)))
-    return new_mat
+    return [list(map((lambda n : n * n),i))for i in matrix]
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+class Square:
+    """represent"""
+
+    def __init__(self, size):
+        """inti
+
+        Args:
+            size: size
+        """
+        self.__size = size
