4ca7b9c8308104717ce526e2580b152da1cd6fee
Yousef eslam <yousefeslam214@gmail.com>
2023-12-27 19:13:56 +0200

    done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ define class """
 
-
 class Square:
     """represntation"""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""define class"""
+
+
+class Square:
+    """represent"""
+    
+    def __init__(self, size):
+        """inti
+        
+        Args:
+            size: size
+        """
+        self.__size = size
