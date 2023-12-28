afd8fcf309834cb1edcf7ad7f9aa97a77761dc6d
Yousef eslam <yousefeslam214@gmail.com>
2023-12-28 08:02:26 +0200

    done

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,12 +15,12 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     @property
     def size(self):
         """size getter"""
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         """size setter"""
@@ -29,6 +29,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         return self.__size * self.__size
