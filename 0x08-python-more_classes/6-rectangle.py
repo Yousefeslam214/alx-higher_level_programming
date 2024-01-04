96ecf7d810a099c1f5621f0d4ed819d0d59887c1
Yousef eslam <yousefeslam214@gmail.com>
2024-01-04 23:32:50 +0200

    done

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,10 +4,13 @@
 
 class Rectangle:
     """representation"""
+    
     number_of_instances = 0
+    
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
         Rectangle.number_of_instances += 1
 
     @property
