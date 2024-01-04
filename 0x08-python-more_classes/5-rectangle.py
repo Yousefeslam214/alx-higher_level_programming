ab2d3f3b6ab52285ddcc02bcd226e70150ecbfd3
Yousef eslam <yousefeslam214@gmail.com>
2024-01-04 23:25:44 +0200

    done

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" class Rectangle """
+
+
+class Rectangle:
+    """representation"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """print rect"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            string = ""
+            for i in range(self.height):
+                for j in range(self.width):
+                    string += "#"
+                if i < self.height - 1:
+                    string += "\n"
+            return string
+
+    def __repr__(self):
+        """..."""
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
