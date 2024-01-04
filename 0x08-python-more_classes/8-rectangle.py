d0b2b92acd87cd99bd7aedea736f7ae4f958f80a
Yousef eslam <yousefeslam214@gmail.com>
2024-01-04 23:56:32 +0200

    done

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -49,7 +49,6 @@ class Rectangle:
         else:
             return 2 * (self.width + self.height)
 
-
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """"which rec is bigger"""
@@ -58,7 +57,6 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         return (rect_1 if rect_1.area() >= rect_2.area() else rect_2)
-
 
     def __str__(self):
         """print rect"""
