71108057a441a4afcee0a57482275006c3f60e70
Yousef eslam <yousefeslam214@gmail.com>
2024-01-11 03:05:24 +0200

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """function that writes a string to a text file"""
-    with open(filename, encoding="utf-8") as file:
-        file.write(text)
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
