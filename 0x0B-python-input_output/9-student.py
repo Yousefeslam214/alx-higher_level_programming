04e7f98d67a0887236530cb6588e92294eec0ee6
Yousef eslam <yousefeslam214@gmail.com>
2024-01-20 07:55:44 +0200

    done

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,6 +3,7 @@
 My class module
 """
 
+
 class Student:
     """ Student """
     def __init__(self, first_name, last_name, age):
@@ -10,8 +11,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
-    
+
     def to_json(self):
         """to json"""
         return self.__dict__
