6ba32ac87c292cf5cc95db1da47876b8a0b12cc9
Yousef eslam <yousefeslam214@gmail.com>
2024-01-21 05:55:43 +0200

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Rectangle """
 
+
 from models.base import Base
+
 
 class Rectangle (Base):
     """Rectangle class"""
@@ -11,60 +13,59 @@ class Rectangle (Base):
         self.x = x
         self.y = y
         super().__init__(id)
-    
+
     @property
     def width(self):
         """get width"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """set width"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """get height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """set height"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-    
+
     @property
     def x(self):
         """get x"""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """set x"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be > 0")
         self.__x = value
-    
+
     @property
     def y(self):
         """get y"""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """set y"""
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be > 0")
         self.__y = value
-    
