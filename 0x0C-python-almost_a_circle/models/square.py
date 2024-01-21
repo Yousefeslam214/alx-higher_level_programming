1ce23f25eb725ceaadf3ee452b4d6ceea908c21f
Yousef eslam <yousefeslam214@gmail.com>
2024-01-21 07:13:11 +0200

    done

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""square"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """init"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
