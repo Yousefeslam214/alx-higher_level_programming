b01fbc78009a6796d435bd3bc6b3e4ed9194d0fe
Yousef eslam <yousefeslam214@gmail.com>
2024-01-22 03:52:10 +0200

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,6 +38,7 @@ class Base:
     def from_json_string(json_string):
         """fun"""
         if json_string is None or json_string == "[]":
-            return "[]"
+            return []
         else:
             return json.loads(json_string)
+
