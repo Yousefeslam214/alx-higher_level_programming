1b88a2b0c8a7b1634f697accf9eb59ec25cbef7e
Yousef eslam <yousefeslam214@gmail.com>
2024-03-12 09:55:18 +0200

    8-task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 if (isNaN(args[0])) {
-  console.log('School');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < args[0]; i++) {
     for (let i = 0; i < args[0]; i++) {
