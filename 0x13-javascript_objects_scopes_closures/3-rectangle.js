1f281fde53c302a61ef714045435b2226f21e0c6
Yousef eslam <yousefeslam214@gmail.com>
2024-03-12 14:44:43 +0200

    3-task

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      for (let i = 0; i < this.width; i++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+}
+
+module.exports = Rectangle;
