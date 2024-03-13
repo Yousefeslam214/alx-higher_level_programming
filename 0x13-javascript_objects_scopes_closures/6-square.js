8b3d14afd96decef33ce8cbbf58d312c02d40a0a
Yousef eslam <yousefeslam214@gmail.com>
2024-03-13 04:37:09 +0200

    6-task

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const Csquare = require('./5-square');
 
-class Square extends Csquare{
-  charPrint(c = 'X') {
+class Square extends Csquare {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write(c);
       }
-    console.log();
+      console.log();
     }
   }
 }
