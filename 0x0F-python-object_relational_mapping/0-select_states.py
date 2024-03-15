4d69ffd4821609817e08d5b40e1a5cdc4440e49a
Yousef eslam <yousefeslam214@gmail.com>
2024-03-15 13:27:26 +0200

    0-task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import mysql.connector
 
-try:
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="hbtn_0e_0_usa"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute(" SELECT * FROM states; ")
-    for i in mycursor:
-        print(i)
-except Exception as e:
-    print("Error:", e)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="hbtn_0e_0_usa"
+)
+mycursor = mydb.cursor()
+mycursor.execute(" SELECT * FROM states; ")
+for i in mycursor:
+    print(i)
