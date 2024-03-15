b682fccf7031221a892927a3318238b8f9f856e3
Yousef eslam <yousefeslam214@gmail.com>
2024-03-15 14:36:00 +0200

    2-task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that lists all states
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    """
+    Access to the database
+    """
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    for states in c.fetchall():
+        if states[1] == sys.argv[4]:
+            print(states)
+    c.close()
+    db.close()
