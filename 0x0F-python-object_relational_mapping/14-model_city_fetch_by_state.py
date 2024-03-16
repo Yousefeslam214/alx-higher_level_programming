5115d0f7044e92bcdc20584e0f512d076c5e1878
Yousef eslam <yousefeslam214@gmail.com>
2024-03-16 11:17:24 +0200

    14-task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,10 +5,8 @@
 #                                          <database name>
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import State,Base
 from model_city import City
-from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Module for City class definition"""
+"""class definition of a City"""
+
 
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
+from model_state import Base
 
 
 class City(Base):
-    """City class"""
-
+    """
+    cities table:
+        id: primary key
+        name: name of the city
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)    
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
