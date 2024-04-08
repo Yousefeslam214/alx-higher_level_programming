e3dad6c5e0f744f946f6e167112dd10744366481
Yousef eslam <yousefeslam214@gmail.com>
2024-04-08 12:31:11 +0200

    2-task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-  console.log('Argument found');
-} else {
+if (process.argv.length === 2) {
   console.log('No argument');
+} else {
+  console.log('Argument found');
 }
