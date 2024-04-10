cbb6a5b073e375a01fcfacd3695321a701e0da9d
Yousef eslam <yousefeslam214@gmail.com>
2024-04-10 17:22:45 +0200

    8-task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-if (!process.argv[2] || isNaN(process.argv[2])){
-    console.log("Missing size")
-}else {
-    for(let i = 0;i < process.argv[2];i++){
-        let x = ''
-        for(let j = 0;j < process.argv[2];j++){
-            x += "X"
-        }
-        console.log(x);
+
+if (!process.argv[2] || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    let x = '';
+    for (let j = 0; j < process.argv[2]; j++) {
+      x += 'X';
     }
-}    
+    console.log(x);
+  }
+}
