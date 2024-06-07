b2992b2182890d6c014e3131b332837c300f88c8
Yousef eslam <yousefeslam214@gmail.com>
2024-06-07 05:10:27 +0300

    4-task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 
-// const request = require('request');
 const request = require('request');
+// const request = require("request");
 const url = process.argv[2];
 
 request.get(url, (error, resource, body) => {
   if (error) {
     console.log(error);
   } else {
-    body = JSON.parse(body)
+    body = JSON.parse(body);
     const films = body.results;
-    console.log(countMoviesWithId(18, films))
+    console.log(countMoviesWithId(18, films));
   }
 });
 
-
-function countMoviesWithId(id, films) {
+function countMoviesWithId (id, films) {
   let count = 0;
   for (const film of films) {
     for (const character of film.characters) {
