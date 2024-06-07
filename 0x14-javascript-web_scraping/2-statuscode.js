be11761da6519450e9c67458b39ce14e040fc26a
Yousef eslam <yousefeslam214@gmail.com>
2024-06-07 04:36:52 +0300

    2-task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, resource, body) => {
-    if (error) {
-        console.log(`code: ${resource.statusCode}`);
-        return;
-    }
+  if (error) {
     console.log(`code: ${resource.statusCode}`);
+    return;
+  }
+  console.log(`code: ${resource.statusCode}`);
 });
