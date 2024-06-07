b91dc63fff4ed8edde55ea01c9045b053d1c369e
Yousef eslam <yousefeslam214@gmail.com>
2024-06-07 05:34:30 +0300

    6-task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,19 +7,17 @@ request.get(url, (error, resource, body) => {
   if (error) {
     console.error(error);
   }
-
   const todoUser = JSON.parse(body);
   const completedTasks = {};
   for (const todo of todoUser) {
     if (todo.completed) {
       const userId = todo.userId;
-      console.log(completedTasks[userId])
       if (completedTasks[userId]) {
-        completedTasks[userId] += 1
+        completedTasks[userId] += 1;
       } else {
-        completedTasks[userId] = 1
+        completedTasks[userId] = 1;
       }
     }
   }
-  console.log(completedTasks)
+  console.log(completedTasks);
 });
